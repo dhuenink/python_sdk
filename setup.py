85616f97ab09b30d2b8d80caf8dd9fec13abcdb1
Mike McLaughlin <mike@aviatrix.com>
2018-05-15 14:33:41 -0500

    updated version to 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='aviatrix-sdk',
-    version='0.1',
+    version='0.3',
     description='Aviatrix Python SDK',
     long_description='Aviatrix Python SDK for automating Aviatrix Controller.',
     author='Aviatrix Solution Architects',
